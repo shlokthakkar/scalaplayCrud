14993a368153878d5b98976c66b34ab4de2cd478
shlokthakkar <58928351+shlokthakkar@users.noreply.github.com>
2023-04-28 16:53:38 +0530

    Testing Added

--- test/controllers/HomeControllerSpec.scala
+++ test/controllers/HomeControllerSpec.scala
@@ -4,7 +4,6 @@ import org.scalatestplus.play._
 import org.scalatestplus.play.guice._
 import play.api.test._
 import play.api.test.Helpers._
-
 /**
  * Add your spec here.
  * You can mock out a whole application including requests, plugins etc.
@@ -15,31 +14,28 @@ class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting
 
   "HomeController GET" should {
 
-    "render the index page from a new instance of controller" in {
-      val controller = new HomeController(stubControllerComponents())
-      val home = controller.index().apply(FakeRequest(GET, "/"))
+    "get all employees in TGT" in {
+      val controller = app.injector.instanceOf(classOf[HomeController])
+//      val dbc =  DatabaseConfigProvider
+//      val dao = new EmployeeDAO(dbc)
+//      val service = new EmployeeService(dao)
+//      val controller = new HomeController(Helpers.stubControllerComponents(),service)
+      val req = controller.getEmployee("TGT").apply(FakeRequest(GET, "/Employee/:company"))
 
-      status(home) mustBe OK
-      contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
+      status(req) mustBe OK
+      contentType(req) mustBe Some("application/json")
     }
 
-    "render the index page from the application" in {
-      val controller = inject[HomeController]
-      val home = controller.index().apply(FakeRequest(GET, "/"))
+    "not found Exception for get all Employees for Samsung" in {
+      val controller = app.injector.instanceOf(classOf[HomeController])
+      //      val dbc =  DatabaseConfigProvider
+      //      val dao = new EmployeeDAO(dbc)
+      //      val service = new EmployeeService(dao)
+      //      val controller = new HomeController(Helpers.stubControllerComponents(),service)
+      val home = controller.getEmployee("Samsung").apply(FakeRequest(GET, "/Employee/:company"))
 
-      status(home) mustBe OK
-      contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
-    }
-
-    "render the index page from the router" in {
-      val request = FakeRequest(GET, "/")
-      val home = route(app, request).get
-
-      status(home) mustBe OK
-      contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
+      status(home) mustBe NOT_FOUND
+      contentType(home) mustBe Some("text/plain")
     }
   }
 }
